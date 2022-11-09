e557cdb623c751b377d26ca4eb555263f7f80e7a
Etherian <etherain@gmail.com>
2022-11-09 16:55:02 -0500

    patch: change default range of normalize99

--- cellpose/transforms.py
+++ cellpose/transforms.py
@@ -185,13 +185,9 @@ def make_tiles(imgi, bsize=224, augment=False, tile_overlap=0.1):
         
     return IMG, ysub, xsub, Ly, Lx
 
-def normalize99(Y, lower=1,upper=99):
-    """ normalize image so 0.0 is 1st percentile and 1.0 is 99th percentile """
-    X = Y.copy()
-    x01 = np.percentile(X, lower)
-    x99 = np.percentile(X, upper)
-    X = (X - x01) / (x99 - x01)
-    return X
+def normalize99(X, lower=.01,upper=99.99):
+    """ normalize image so 0.0 is `lower`th percentile and 1.0 is `higher`th percentile """
+    return np.interp(X, (np.percentile(X, lower), np.percentile(X, upper)), (0, 1))
 
 def move_axis(img, m_axis=-1, first=True):
     """ move axis m_axis to first or last position """
